6e381e9ebe33b9d6570aaaf707ecce657db5fe79
nyamvularinga <nyamvularinga@gmail.com>
2023-04-28 11:16:03 +0300

    Complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
 
+  function fetchedData() {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((data) => setQuestions(data));
+  }
+
+  useEffect(() => {
+    fetchedData();
+  }, []);
+
+  // addQuestion adds one more question to the ui after submit
+  function addQuestion(data) {
+    setQuestions((prevState) => [...prevState, data]);
+  }
+
+  function onDeleteQuestion(id) {
+    setQuestions((prevState) => prevState.filter((item) => item.id !== id));
+  }
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm addQuestion={addQuestion} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDeleteQuestion={onDeleteQuestion}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,6 +8,11 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    }).then(() => onDeleteQuestion(id));
+  }
 
   return (
     <li>
@@ -15,9 +20,17 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select
+          defaultValue={correctIndex}
+        >
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button
+      onClick={handleDelete}
+      >
+        Delete Question
+      </button>
     </li>
   );
 }
